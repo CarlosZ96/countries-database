fe96170d84aad9248db0eb854ff384e7581c4963
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-09-20 20:37:54 -0500

    Feature:
    Adding event listener to filter by country name

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,34 +1,41 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
+import { nanoid } from '@reduxjs/toolkit';
 import { getCountires } from '../redux/countries/countrieSlice';
-import '../stylesheets/countries.css';
 
-function CountiresItem() {
+function CountryDetails(Name) {
   const dispatch = useDispatch();
   const { countries } = useSelector((store) => store.countries);
   useEffect(() => {
     dispatch(getCountires());
   }, [dispatch]);
 
-  const filter = countries.filter(
-    (acountrie) => acountrie.name === 'United States'
-      || acountrie.name === 'United Kingdom'
-      || acountrie.name === 'China'
-      || acountrie.name === 'Australia'
-      || acountrie.name === 'Mexico'
-      || acountrie.name === 'Colombia',
+  const filterName = countries.filter(
+    (acountrie) => acountrie.name === Name,
   );
   return (
-    <div className="countires-container">
+    <div>
       {
-        filter.map(
+        filterName.map(
           (countrie) => (
             <div key={nanoid()} className="countrie-card">
               <div className="flag-container">
                 <img src={countrie.flags} alt={countrie.alt} className="flag" />
               </div>
               <h1>{countrie.name}</h1>
+              <h2>
+                Continent:
+                {countrie.continent}
+              </h2>
+              <h2>
+                Capital:
+                {countrie.capital}
+              </h2>
+              <h2>
+                Area:
+                {countrie.area}
+              </h2>
               <h3>
                 Population:
                 {countrie.population / 1000000}
@@ -41,4 +48,8 @@ function CountiresItem() {
   );
 }
 
-export default CountiresItem;
+CountryDetails.propTypes = {
+  Name: PropTypes.string.isRequired,
+};
+
+export default CountryDetails;
